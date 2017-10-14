daa5cf4276f0f10b5d578c7bfffe5dcc20a9c184
Philippe Rigovanov <phil@rigovanov.ru>
2017-10-14 10:58:46 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,3 @@
-(function($, undefined) {
-    $.extend({
-        toUTF8     : function(text) {
-            text = text.replace(/\r\n/g,"\n");
-
-            var output = [];
-
-            for (var n = 0 ; n < text.length ; n++) {
-                var c = text.charCodeAt(n);
-
-                if (c < 128) {
-                    output.push(String.fromCharCode(c));
-                } else if ((c > 127) && (c < 2048)) {
-                    output.push(String.fromCharCode((c >> 6) | 192));
-                    output.push(String.fromCharCode((c & 63) | 128));
-                } else {
-                    output.push(String.fromCharCode((c >> 12) | 224));
-                    output.push(String.fromCharCode(((c >> 6) & 63) | 128));
-                    output.push(String.fromCharCode((c & 63) | 128));
-                }
-            }
-
-            return output.join('');
-        },
-        fromUTF8   : function(text) {
-            var output = [],
-                i = c = c1 = c2 = 0;
-
-            while (i < text.length) {
-                c = text.charCodeAt(i);
-
-                if (c < 128) {
-                    output.push(String.fromCharCode(c));
-                    i++;
-                } else if ((c > 191) && (c < 224)) {
-                    c2 = text.charCodeAt(i + 1);
-                    output.push(String.fromCharCode(((c & 31) << 6) | (c2 & 63)));
-                    i += 2;
-                } else {
-                    c2 = text.charCodeAt(i + 1);
-                    c3 = text.charCodeAt(i + 2);
-                    output.push(String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63)));
-                    i += 3;
-                }
-            }
-
-            return output.join('');
-        }
-    });
-})(jQuery);
-
 var sectionHeight = function() {
   var total    = $(window).height(),
       $section = $('section').css('height','auto');
@@ -65,8 +14,8 @@ $(window).resize(sectionHeight);
 
 $(document).ready(function(){
   $("section h1, section h2").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
+    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w\u0410-\u044F-]+/g,'') + "'>" + $(this).text() + "</a></li>");
+    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w\u0410-\u044F-]+/g,''));
     $("nav ul li:first-child a").parent().addClass("active");
   });
 
